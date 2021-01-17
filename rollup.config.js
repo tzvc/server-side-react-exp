d8163f9d486420594aa53985661b3459b16b70c4
theochampion <contact.theochampion@gmail.com>
2021-01-17 19:41:48 +0100

    :hammer: Added Rollup JS build config

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  // {
+  //   input: 'src/main.ts',
+  //   output: {
+  //     name: 'howLongUntilLunch',
+  //     file: pkg.browser,
+  //     format: 'umd',
+  //   },
+  //   plugins: [
+  //     resolve(), // so Rollup can find `ms`
+  //     commonjs(), // so Rollup can convert `ms` to an ES module
+  //     typescript(), // so Rollup can convert TypeScript to JavaScript
+  //     babel({
+  //       exclude: 'node_modules/**',
+  //       presets: ['@babel/env', '@babel/preset-react'],
+  //     }),
+  //   ],
+  // },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/main.tsx',
+    external: ['react-dom/server', 'react'],
+    plugins: [
+      typescript(), // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+];
